1cc15c5498c894585b6625237b522b654cf41db7
rahulbansal <bansalrahul194@gmail.com>
2024-08-09 12:06:21 +0530

    init

---
+++ app/_components/Navigation.js
@@ -0,0 +1,34 @@
+import Link from "next/link"; // Note the import statement change
+
+export default function Navigation() {
+  return (
+    <nav className="z-10 text-xl">
+      <ul className="flex gap-16">
+        <li>
+          <Link
+            href="/cabins"
+            className="hover:text-accent-400 transition-colors"
+          >
+            Cabins
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/about"
+            className="hover:text-accent-400 transition-colors"
+          >
+            About
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/account"
+            className="hover:text-accent-400 transition-colors"
+          >
+            Guest area
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
